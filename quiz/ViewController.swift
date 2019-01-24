4e825bd495ed46398a0e373fa007d4bd4b9c2665
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-01-23 22:26:26 -0600

    QA

--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerLabel: UILabel!
     
+    let questions: [String] = [
+        "what is 7+7",
+        "what is the capital of VT",
+        "what is cognac made fron?"
+    ]
+    let answers: [String] = [
+     "14",
+     "Montpelier",
+     "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(_ sender: Any) {
     }
     @IBAction func showAnswer(_ sender: Any) {
